4920662a7978af1d05d03ae272ad6af45b9a40a4
Kyungwon Chun <ruddyscent@gmail.com>
2011-08-09 02:47:49 +0000

    * file name change: from parallel_pi_v?.f90 to ppi_v?.f90, respectively. * make configuration to compile f90 codes. * Added new f90 code descriptions at the README file.

--- Makefile
+++ Makefile
@@ -1,21 +1,25 @@
-.SUFFIXES : .c .o
+.SUFFIXES : .c .f90
 
 CC = mpicc
+FC = mpif90
 
-INCLUDE = 
-CFLAGS = -g -Wall -std=c99 $(INCLUDE)
+CFLAGS = -g -Wall -std=c99
+FFLAGS = -g -Wall
 LIBS = -lm
 LDFLAGS = $(LIBS)
 
-TARGET = cpi icpi cdf pi_montecarlo
+TARGET = cpi icpi cdf pi_montecarlo cpi2 ppi_v1 ppi_v2
 
 all : $(TARGET)
 
+.f90:
+	$(FC) $(FFLAGS) -o $@ $< 
+
 clean :
-	rm -rf $(OBJS) $(TARGET) core
+	rm -rf $(OBJS) $(TARGET) *.o core
 
 clean_all :
-	rm -rf $(OBJS) $(TARGET) *~ core
+	rm -rf $(OBJS) $(TARGET) *.o core *~
 
 new :
 	$(MAKE) clean
--- cpi2.c
+++ cpi2.c
@@ -1,10 +1,13 @@
-/* cpi2.c - C version of the Fortran90 code calculating the value of Pi in parallel written in the USC parallel programming camp.
- * Author: Huioon Kim, pcandme@gist.ac.kr
- * Last modified by Huioon Kim, 2011.8.4
+/* 
+ * cpi2.c - C version of the Fortran90 code calculating Pi in parallel written 
+ * in the USC parallel programming camp.
+ *
+ * Huioon Kim (pcandme@gist.ac.kr)
  */
 
 #include "mpi.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
 #define min(a, b) (((a)<(b))? (a):(b))
@@ -13,52 +16,49 @@
 
 int main(int argc, char *argv[])
 {
-    int myrank, nprocs, i, ista, iend, remain, stride;
-    double sum, step, local_pi, pi, x, stime, etime;
+  int myrank, nprocs, i, ista, iend, remain, stride;
+  double sum, step, local_pi, pi, x, stime, etime;
 
-    MPI_Init(&argc, &argv);
-    MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
-    MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+  MPI_Init(&argc, &argv);
+  MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+  MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
 
-    stride = NUM_STEP / nprocs;
-    remain = NUM_STEP % nprocs;
+  stride = NUM_STEP / nprocs;
+  remain = NUM_STEP % nprocs;
 
-    ista = myrank * stride + min(myrank, remain) + 1;
-    iend = ista + stride - 1;
+  ista = myrank * stride + min(myrank, remain) + 1;
+  iend = ista + stride - 1;
 
-    if (remain > myrank) iend = iend + 1;
+  if (remain > myrank) 
+    iend = iend + 1;
 
-    step = (1.0 / NUM_STEP);
+  step = (1.0 / NUM_STEP);
+    
+  sum = 0.0;
 
-    sum = 0.0;
+  if (myrank == 0)
+    printf("----------------------------------------------\n");
 
-    if (myrank == 0)
-    {
-        printf("----------------------------------------------\n");
-    }
+  stime = MPI_Wtime();
 
-    stime = MPI_Wtime();
+  for (i = ista; i <= iend; i++) {
+    x = (i - 0.5) * step;
+    sum = sum + 4.0 / (1.0 + x * x);
+  }
 
-    for (i = ista; i <= iend; i++)
-    {
-        x = (i - 0.5) * step;
-        sum = sum + 4.0 / (1.0 + x * x);
-    }
+  local_pi = step * sum;
 
-    local_pi = step * sum;
+  MPI_Reduce(&local_pi, &pi, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
 
-    MPI_Reduce(&local_pi, &pi, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+  etime = MPI_Wtime();
 
-    etime = MPI_Wtime();
+  if (myrank == 0) {
+    printf(" Pi = %17.15f (Error = %11.5e)\n", pi, fabs(acos(-1.0) - pi));
+    printf(" Elapsed Time = %8.4f [sec]\n", etime - stime);
+    printf("----------------------------------------------\n");
+  }
 
-    if (myrank == 0)
-    {
-        printf(" Pi = %17.15f (Error = %11.5e)\n", pi, fabs(acos(-1.0) - pi));
-        printf(" Elapsed Time = %8.4f [sec]\n", etime - stime);
-        printf("----------------------------------------------\n");
-    }
+  MPI_Finalize();
 
-    MPI_Finalize();
-
-    return 0;
+  return EXIT_SUCCESS;
 }
